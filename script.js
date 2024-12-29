2979adc577741d26856b9975d6cdaacf2860724e
Amjad <amjadabdulmajeed8@gmail.com>
2024-12-29 22:48:28 +0100

    Solve the challenge_2

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-//Challenge 1
-const arr = [1, 2, 3, 4, 5];
+//Challenge 2
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 6, 7, 8, 9, 10];
+
+//Solution 1
+const arr3 = arr1.slice(0, 4).concat(arr2);
+console.log(arr3);
+
+//Solution 2
+const arr4 = [...arr1, ...arr2];
+x = arr3.splice(4, 1);
 
 
-arr.reverse();
-arr.push(0);
-arr.unshift(6);
-
-
-console.log(arr);
+console.log(arr3);
+console.log(arr4);
